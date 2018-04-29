2ef4bb5075ecb04f84cd48d6dda656b9e6bd5009
Alexandre Abruzzese <alexandre.abruzzese@ynov.com>
2018-04-29 11:08:47 +0200

    final commit

--- main.js
+++ main.js
@@ -8,28 +8,69 @@ var i = 1
 var concTabQuestion = {}
 var concTabReponse = {}
 
-var parsing = axios.get('https://opentdb.com/api.php?amount=10')
-.then(function(response){
-  for(item of response.data.results) {
-    concQuestion = concQuestion + "//" + item.question
-    concReponse = concReponse + "//" + item.correct_answer
-    concTabQuestion = concQuestion.split("//")
-    concTabReponse = concReponse.split("//")
-    }
-    while (i < concTabQuestion.length)
+function quizz(str)
+{
+  var parsing = axios.get('https://opentdb.com/api.php?amount=10')
+  .then(function(response){
+    for(item of response.data.results) {
+      concQuestion = concQuestion + "//" + item.question
+      concReponse = concReponse + "//" + item.correct_answer
+      concTabQuestion = concQuestion.split("//")
+      concTabReponse = concReponse.split("//")
+      }
+      if (str != null)
+        console.log(str)
+      while (i < concTabQuestion.length)
+      {
+        console.log(concTabQuestion[i])
+        const answer = readline.question("What is the answer ?\n")
+        if (answer == concTabReponse[i])
+        {
+          console.log("Find ! Next !")
+          count = count + 1
+        }
+        else
+        {
+          console.log('Fail ! Next !')
+        }
+        i++
+      }
+      console.log('You have ' + count + ' points')
+    });
+    return count
+}
+
+function scores(score1, score2)
+{
+  if (score1 > score2)
+    console.log("Player 1 win with " + score1 + " points")
+  else
     {
-      console.log(concTabQuestion[i])
-      const answer = readline.question("What is the answer ?\n")
-      if (answer == concTabReponse[i])
-      {
-        console.log("Find ! Next !")
-        count = count + 1;
-      }
-      else
-      {
-        console.log('Fail ! Next !')
-      }
-      i++
+      console.log("Player 2 win with " + score2 + " points")
     }
-    console.log('You have ' + count + ' points')
-  });
+}
+
+function manuel()
+{
+  console.log('If you want to play solo, just use \"-solo\"')
+  console.log('If you want to play with a friend, just use \"-versus\"')
+}
+async function choix()
+{
+  if (process.argv[2] == "-solo")
+  {
+    quizz()
+  }
+  else if (process.argv[2] == "-versus")
+  {
+    const score1 = await quizz("Player 1 begin !")
+    const score2 = await quizz("Player 2 begin !")
+    await scores(score1, score2)
+  }
+  else if (process.argv[2] == "-h")
+  {
+    manuel()
+  }
+}
+
+choix()
