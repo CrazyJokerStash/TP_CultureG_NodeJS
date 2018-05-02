6d0f18e0ecbe20512004ed754aa6ce9d35a02571
Alexandre Abruzzese <alexandre.abruzzese@ynov.com>
2018-05-02 09:50:19 +0200

    Final commit

--- main.js
+++ main.js
@@ -7,51 +7,57 @@ var count = 0
 var i = 1
 var concTabQuestion = {}
 var concTabReponse = {}
+var Promise = require('es6-promise').Promise;
 
 async function quizz(str)
 {
-  var parsing = axios.get('https://opentdb.com/api.php?amount=10')
-  .then(function(response){
-    try {
-      for(item of response.data.results) {
-      concQuestion = concQuestion + "//" + item.question
-      concReponse = concReponse + "//" + item.correct_answer
-      concTabQuestion = concQuestion.split("//")
-      concTabReponse = concReponse.split("//")
-      }
-      if (str != null)
-        console.log(str)
-      while (i < concTabQuestion.length)
-      {
-        console.log(concTabQuestion[i])
-        const answer = readline.question("What is the answer ?\n")
-        if (answer == concTabReponse[i])
-        {
-          console.log("Find ! Next !")
-          count = count + 1
+  return new Promise(function(resolve, reject)
+  {
+    count = 0
+    var parsing = axios.get('https://opentdb.com/api.php?amount=10')
+    .then(function(response){
+      try {
+        for(item of response.data.results) {
+          concQuestion = concQuestion + "//" + item.question
+          concReponse = concReponse + "//" + item.correct_answer
+          concTabQuestion = concQuestion.split("//")
+          concTabReponse = concReponse.split("//")
         }
-        else
-        {
-          console.log('Fail ! Next !')
+        if (str != null)
+          console.log(str)
+          while (i < concTabQuestion.length)
+          {
+            console.log(concTabQuestion[i])
+            console.log(concTabReponse[i])
+            const answer = readline.question("What is the answer ?\n")
+          if (answer == concTabReponse[i])
+          {
+            console.log("Find ! Next !\n")
+            count = count + 1
+          }
+          else
+          {
+            console.log('Fail ! Next !\n')
+          }
+          i++
         }
-        i++
+        console.log('You have ' + count + ' points\n')
+        resolve(count)
       }
-      console.log('You have ' + count + ' points')
-    }
-    catch(error){
-      console.error(error)
-    }
-    });
-    return count
+      catch(error){
+        console.error(error)
+      }
+      });
+  })
 }
 
 function scores(score1, score2)
 {
   if (score1 > score2)
-    console.log("Player 1 win with " + score1 + " points")
+    console.log("Player 1 win with " + score1 + " points\n")
   else
     {
-      console.log("Player 2 win with " + score2 + " points")
+      console.log("Player 2 win with " + score2 + " points\n")
     }
 }
 
@@ -69,8 +75,8 @@ async function choix()
   }
   else if (process.argv[2] == "-versus")
   {
-    const score1 = await quizz("Player 1 begin !")
-    const score2 = await quizz("Player 2 begin !")
+    var score1 = await quizz("Player 1 begin !\n")
+    var score2 = await quizz("Player 2 begin !\n")
     scores(score1, score2)
   }
   else if (process.argv[2] == "-h")
